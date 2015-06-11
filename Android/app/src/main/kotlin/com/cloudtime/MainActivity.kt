96a72c296d6659a7182915a7d37d9493c609b94b
Maciej Górski <maciek.gorski@gmail.com>
2015-06-12 00:38:50 +0200

    Push object to Parse on Activity creation.

--- Android/app/src/main/kotlin/com/cloudtime/MainActivity.kt
+++ Android/app/src/main/kotlin/com/cloudtime/MainActivity.kt
@@ -2,11 +2,18 @@ package com.cloudtime
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.parse.ParseObject
+import com.parse.SaveCallback
 
 public class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val obj = ParseObject("MySecondClass")
+        obj.put("myFirstField", 6)
+        obj.saveInBackground()
     }
 }

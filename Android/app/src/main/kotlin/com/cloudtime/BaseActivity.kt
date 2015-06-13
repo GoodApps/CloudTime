3ebb9b2e2ffc3fb0569036fa9be87932fab8ab44
Maciej Górski <maciek.gorski@gmail.com>
2015-06-13 17:08:44 +0200

    Add empty Login screen.

---
+++ Android/app/src/main/kotlin/com/cloudtime/BaseActivity.kt
@@ -0,0 +1,6 @@
+package com.cloudtime
+
+import android.support.v7.app.AppCompatActivity
+
+public open class BaseActivity : AppCompatActivity() {
+}
---
+++ Android/app/src/main/kotlin/com/cloudtime/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.cloudtime
+
+import android.os.Bundle
+
+public class LoginActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_activity)
+    }
+}

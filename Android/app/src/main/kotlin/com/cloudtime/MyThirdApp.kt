5b61f5862d1d53bbfb24bbc5ba051b2df8e0cbe8
Maciej Górski <maciek.gorski@gmail.com>
2015-06-14 12:48:10 +0200

    Push messages support.

--- Android/app/src/main/kotlin/com/cloudtime/MyThirdApp.kt
+++ Android/app/src/main/kotlin/com/cloudtime/MyThirdApp.kt
@@ -3,6 +3,7 @@ package com.cloudtime
 import android.app.Application
 import com.parse.Parse
 import com.parse.ParseACL
+import com.parse.ParseInstallation
 import com.parse.ParseUser
 
 public class MyThirdApp : Application() {
@@ -16,11 +17,18 @@ public class MyThirdApp : Application() {
         Parse.initialize(this, "pL0PLrNyjePVlFJa1p1siq84Pk0qJiy8EuzLbBxF", "UTvNS997O35LIH4GqH2uIFvwer9Sx5KZWM0ZxTDQ")
         ParseUser.enableAutomaticUser()
         setDefaultACL()
+        initInstallation()
     }
 
     private fun setDefaultACL() {
         val withWriteAccessForCurrentUser = true
         val noPermissionsACL = ParseACL()
         ParseACL.setDefaultACL(noPermissionsACL, withWriteAccessForCurrentUser)
+    }
+
+    private fun initInstallation() {
+        val installation = ParseInstallation.getCurrentInstallation()
+        installation.put("user", ParseUser.getCurrentUser())
+        installation.saveInBackground()
     }
 }

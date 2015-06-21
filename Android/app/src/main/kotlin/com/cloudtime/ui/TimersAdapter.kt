024fae5495fa875ba4679586c04694dcd0eacc7e
Karol <KD@Karols-MacBook-Pro.local>
2015-06-21 15:24:51 +0200

    Fixed an issue where only last item was updated on tick (need to capture var to val)

--- Android/app/src/main/kotlin/com/cloudtime/ui/TimersAdapter.kt
+++ Android/app/src/main/kotlin/com/cloudtime/ui/TimersAdapter.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.os.CountDownTimer
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import com.cloudtime.dto.Timer
 import com.cloudtime.ui.common.BaseRecyclerAdapter
 import java.util.HashSet
@@ -27,12 +28,14 @@ public class TimersAdapter(
         var itemPos = -1
         timers.forEach {
             itemPos ++;
+            val itemPosVal = itemPos
             addItem(TimerItemAdapter(it, activity))
             if ( it.started() ) {
-                val countDownTimer = object : CountDownTimer(Long.MAX_VALUE, 250) {
+                val countDownTimer = object : CountDownTimer(Long.MAX_VALUE, 500) {
 
                     override fun onTick(millisUntilFinished: Long) {
-                        notifyItemChanged(itemPos)
+                        notifyItemChanged(itemPosVal)
+//                        Log.d("TimersAdapter", "notifyItemChanged: $itemPos")
 //                        notifyDataSetChanged()
                     }
 

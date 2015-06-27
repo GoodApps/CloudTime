d83ec2bf505b31b020a1bc23a4c938c92937e356
Karol <KD@Karols-MacBook-Pro.local>
2015-06-27 18:56:54 +0200

    Prevent crashing, by setting params to optional ("Intent?"). This is a recurring trap in Kotlin that we should watch out for!

--- Android/app/src/main/kotlin/com/cloudtime/alarm/AlarmAndroidService.kt
+++ Android/app/src/main/kotlin/com/cloudtime/alarm/AlarmAndroidService.kt
@@ -13,23 +13,23 @@ import com.cloudtime.service.TimerService
 
 public class AlarmAndroidService : Service() {
 
-    override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         TimerService().loadTimers()
                 .subscribe({ createAlarm(it, intent) }, { handleError(it, intent) })
         return Service.START_STICKY
     }
 
-    private fun createAlarm(timers: List<Timer>, intent: Intent) {
+    private fun createAlarm(timers: List<Timer>, intent: Intent?) {
         AlarmService(this).createAlarm(timers)
         WakefulBroadcastReceiver.completeWakefulIntent(intent)
     }
 
-    private fun handleError(t: Throwable, intent: Intent) {
+    private fun handleError(t: Throwable, intent: Intent?) {
         Log.e("tag", "error", t)
         WakefulBroadcastReceiver.completeWakefulIntent(intent)
     }
 
-    override fun onBind(intent: Intent): IBinder? {
+    override fun onBind(intent: Intent?): IBinder? {
         return null
     }
 }
